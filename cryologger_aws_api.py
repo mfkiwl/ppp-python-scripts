14868f5963390e422a9821343aa45fbf54e57a1b
Adam <adam.garbo@gmail.com>
2023-02-22 12:57:02 -0500

    Update cryologger_aws_api.py

--- cryologger_aws_api.py
+++ cryologger_aws_api.py
@@ -50,45 +50,51 @@ path_figures = "/Users/adam/Documents/GitHub/cryologger-automatic-weather-statio
 # Load and prepare data
 # -----------------------------------------------------------------------------
 
-# Qakuqtaqtujut
-response = requests.get("https://api.cryologger.org/aws?imei=300434063398110&records=3000")
-#response = requests.get("https://api.cryologger.org/aws?imei=300434063398110&field=voltage&records=10000")
-df1 = pd.read_json(response.text)
+# Station UID
+# ALW	73.581863	-83.654776	Arctic Bay, Nunavut	Qakuqtaqtujut
+# MPC	73.4802	    -85.6052	Arctic Bay, Nunavut	Pullataujaq
+headers = {'x-api-key': "fZ9d7bKQB69Yur74Xmz7i8Rf7o24kpga4PpnGxBF"}
 
-# Pullataujaq
-response = requests.get("https://api.cryologger.org/aws?imei=300434063396360&records=3000")
-#response = requests.get("https://api.cryologger.org/aws?imei=300434063396360&field=transmit_duration&records=6000")
-df2 = pd.read_json(response.text)
+url = "https://api.cryologger.org/aws?uid=NPK"
+response = requests.get(url, headers=headers)
+df = pd.read_json(response.text)
+response.content
+
+
+url = "https://api.cryologger.org/aws?uid=ALW&uid=MPC&records=200000"
+response = requests.get(url, headers=headers)
+df = pd.read_json(response.text)
 
 # Convert unixtime to datetime
-df1["datetime"] = pd.to_datetime(df1["unixtime"], unit="s")
-df2["datetime"] = pd.to_datetime(df2["unixtime"], unit="s")
+df["datetime"] = pd.to_datetime(df["unixtime"], unit="s")
 
 # Subset by datetime
-df1 = df1[(df1["datetime"] > "2022-05-09 00:00")]
-df2 = df2[(df2["datetime"] > "2022-05-09 00:00")]
+df = df[(df["datetime"] > "2023-01-01 00:00")]
+
+#df = df[(df["datetime"] < "2023-01-28 00:00")]
 
 # Remove 0 s transmit durations
-df1.loc[df1["transmit_duration"] == 0, "transmit_duration"] = np.nan
-df2.loc[df2["transmit_duration"] == 0, "transmit_duration"] = np.nan
+df.loc[df["transmit_duration"] == 0, "transmit_duration"] = np.nan
 
 # Remove voltages less than 12 V
-df1.loc[df1["voltage"] < 12, "voltage"] = np.nan
-df2.loc[df2["voltage"] < 12, "voltage"] = np.nan
+df.loc[df["voltage"] < 12.5, "voltage"] = np.nan
 
 # Convert wind speed from m/s to km/h
-df1["wind_speed"] *= 3.6 
-df2["wind_speed"] *= 3.6
+df["wind_speed"] *= 3.6 
+
+df["wind_gust_speed"] *= 3.6 
+
+df.loc[df["wind_gust_speed"] == 0, "wind_gust_speed"] = np.nan
 
 # -----------------------------------------------------------------------------
 # Plot
 # -----------------------------------------------------------------------------
 
-# Voltage
+# Voltage & Temperature
+
 fig, ax = plt.subplots(figsize=(10,5))
 ax.grid(ls="dotted")
-sns.lineplot(x="datetime", y="voltage", data=df1, errorbar=None, lw=lw, label="Qakuqtaqtujut")
-sns.lineplot(x="datetime", y="voltage", data=df2, errorbar=None, lw=lw, label="Pullataujaq")
+sns.lineplot(x="datetime", y="voltage", data=df, errorbar=None, lw=lw, hue="uid")
 ax.set(xlabel=None, ylabel="Voltage (V)")
 plt.xticks(rotation=45, horizontalalignment="right")
 ax.xaxis.set_major_formatter(mdates.DateFormatter(date_format))
@@ -99,8 +105,7 @@ plt.savefig(path_figures + "voltage.png", dpi=dpi, transparent=False, bbox_inche
 # Temperature Internal (°C)
 fig, ax = plt.subplots(figsize=(10,5))
 ax.grid(ls="dotted")
-sns.lineplot(x="datetime", y="temperature_int", data=df1, errorbar=None, lw=lw, label="Qakuqtaqtujut")
-sns.lineplot(x="datetime", y="temperature_int", data=df2, errorbar=None, lw=lw, label="Pullataujaq")
+sns.lineplot(x="datetime", y="temperature_int", data=df, errorbar=None, lw=lw, hue="uid")
 ax.set(xlabel=None, ylabel="Temperature (°C)")
 plt.xticks(rotation=45, horizontalalignment="right")
 ax.xaxis.set_major_formatter(mdates.DateFormatter(date_format))
@@ -111,8 +116,7 @@ plt.savefig(path_figures + "temperature_int.png", dpi=dpi, transparent=False, bb
 # Temperature External (°C)
 fig, ax = plt.subplots(figsize=(10,5))
 ax.grid(ls="dotted")
-sns.lineplot(x="datetime", y="temperature_ext", data=df1, errorbar=None, lw=lw, label="Qakuqtaqtujut")
-sns.lineplot(x="datetime", y="temperature_ext", data=df2, errorbar=None, lw=lw, label="Pullataujaq")
+sns.lineplot(x="datetime", y="temperature_ext", data=df, errorbar=None, lw=lw, hue="uid")
 ax.set(xlabel=None, ylabel="Temperature External (°C)")
 plt.xticks(rotation=45, horizontalalignment="right")
 ax.xaxis.set_major_formatter(mdates.DateFormatter(date_format))
@@ -123,8 +127,7 @@ plt.savefig(path_figures + "temperature_ext.png", dpi=dpi, transparent=False, bb
 # Humidity (%)
 fig, ax = plt.subplots(figsize=(10,5))
 ax.grid(ls="dotted")
-sns.lineplot(x="datetime", y="humidity_ext", data=df1, errorbar=None, lw=lw, label="Qakuqtaqtujut")
-sns.lineplot(x="datetime", y="humidity_ext", data=df2, errorbar=None, lw=lw, label="Pullataujaq")
+sns.lineplot(x="datetime", y="humidity_ext", data=df, errorbar=None, lw=lw, label="Qakuqtaqtujut")
 ax.set(xlabel=None, ylabel="Humidity External (%)")
 plt.xticks(rotation=45, horizontalalignment="right")
 ax.xaxis.set_major_formatter(mdates.DateFormatter(date_format))
@@ -135,20 +138,29 @@ plt.savefig(path_figures + "temperature_ext.png", dpi=dpi, transparent=False, bb
 # Wind Speed
 fig, ax = plt.subplots(figsize=(10,5))
 ax.grid(ls="dotted")
-sns.lineplot(x="datetime", y="wind_speed", data=df1, errorbar=None, lw=lw, label="Qakuqtaqtujut")
-sns.lineplot(x="datetime", y="wind_speed", data=df2, errorbar=None, lw=lw, label="Pullataujaq")
+sns.lineplot(x="datetime", y="wind_speed", data=df, errorbar=None, lw=lw, hue="uid")
 ax.set(xlabel=None, ylabel="Wind Speed (km/h)")
 plt.xticks(rotation=45, horizontalalignment="right")
 ax.xaxis.set_major_formatter(mdates.DateFormatter(date_format))
 ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1)) 
 ax.legend(loc="center left", bbox_to_anchor=(1.0, 0.5), title="Station")
-plt.savefig(path_figures + "temperature_int.png", dpi=dpi, transparent=False, bbox_inches="tight")
+plt.savefig(path_figures + "wind_speed.png", dpi=dpi, transparent=False, bbox_inches="tight")
+
+# Wind Gust Speed 
+fig, ax = plt.subplots(figsize=(10,5))
+ax.grid(ls="dotted")
+sns.lineplot(x="datetime", y="wind_gust_speed", data=df, errorbar=None, lw=lw, hue="uid")
+ax.set(xlabel=None, ylabel="Wind Gust Speed (km/h)")
+plt.xticks(rotation=45, horizontalalignment="right")
+ax.xaxis.set_major_formatter(mdates.DateFormatter(date_format))
+ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1)) 
+ax.legend(loc="center left", bbox_to_anchor=(1.0, 0.5), title="Station")
+plt.savefig(path_figures + "wind_gust_speed.png", dpi=dpi, transparent=False, bbox_inches="tight")
 
 # Wind Direction (°)
 fig, ax = plt.subplots(figsize=(10,5))
 ax.grid(ls="dotted")
-sns.lineplot(x="datetime", y="wind_direction", data=df1, errorbar=None, lw=lw, label="Qakuqtaqtujut")
-sns.lineplot(x="datetime", y="wind_direction", data=df2, errorbar=None, lw=lw, label="Pullataujaq")
+sns.lineplot(x="datetime", y="wind_direction", data=df, errorbar=None, lw=lw, hue="uid")
 ax.set(xlabel=None, ylabel="Wind Direction (°)")
 plt.xticks(rotation=45, horizontalalignment="right")
 ax.xaxis.set_major_formatter(mdates.DateFormatter(date_format))
@@ -159,32 +171,29 @@ plt.savefig(path_figures + "temperature_int.png", dpi=dpi, transparent=False, bb
 # Pitch (°)
 fig, ax = plt.subplots(figsize=(10,5))
 ax.grid(ls="dotted")
-sns.lineplot(x="datetime", y="pitch", data=df1, errorbar=None, lw=lw, label="Qakuqtaqtujut")
-sns.lineplot(x="datetime", y="pitch", data=df2, errorbar=None, lw=lw, label="Pullataujaq")
+sns.lineplot(x="datetime", y="pitch", data=df, errorbar=None, lw=lw, hue="uid")
 ax.set(xlabel=None, ylabel="Pitch (°)")
 plt.xticks(rotation=45, horizontalalignment="right")
 ax.xaxis.set_major_formatter(mdates.DateFormatter(date_format))
-ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1)) 
+#ax.xaxis.set_major_locator(mdates.DayLocator(interval=7)) 
 ax.legend(loc="center left", bbox_to_anchor=(1.0, 0.5), title="Station")
 plt.savefig(path_figures + "pitch.png", dpi=dpi, transparent=False, bbox_inches="tight")
 
 # Roll (°)
 fig, ax = plt.subplots(figsize=(10,5))
 ax.grid(ls="dotted")
-sns.lineplot(x="datetime", y="roll", data=df1, errorbar=None, lw=lw, label="Qakuqtaqtujut")
-sns.lineplot(x="datetime", y="roll", data=df2, errorbar=None, lw=lw, label="Pullataujaq")
+sns.lineplot(x="datetime", y="roll", data=df, errosrbar=None, lw=lw, hue="uid")
 ax.set(xlabel=None, ylabel="Roll (°)")
 plt.xticks(rotation=45, horizontalalignment="right")
 ax.xaxis.set_major_formatter(mdates.DateFormatter(date_format))
-ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1)) 
+ax.xaxis.set_major_locator(mdates.DayLocator(interval=7)) 
 ax.legend(loc="center left", bbox_to_anchor=(1.0, 0.5), title="Station")
 plt.savefig(path_figures + "roll.png", dpi=dpi, transparent=False, bbox_inches="tight")
 
 # Transmit Duration
 fig, ax = plt.subplots(figsize=(10,5))
 ax.grid(ls="dotted")
-sns.scatterplot(x="datetime", y="transmit_duration", data=df1, label="Qakuqtaqtujut")
-sns.scatterplot(x="datetime", y="transmit_duration", data=df2, label="Pullataujaq")
+sns.scatterplot(x="datetime", y="transmit_duration", data=df, hue="uid")
 ax.set(xlabel=None, ylabel="Transmit Duration (s)")
 plt.xticks(rotation=45, horizontalalignment="right")
 ax.xaxis.set_major_formatter(mdates.DateFormatter(date_format))
@@ -195,11 +204,43 @@ plt.savefig(path_figures + "transmit_duration.png", dpi=dpi, transparent=False, 
 # Voltage
 fig, ax = plt.subplots(figsize=(10,5))
 ax.grid(ls="dotted")
-sns.lineplot(x="datetime", y="min", data=df3, errorbar=None, lw=lw, label="Qakuqtaqtujut")
-sns.lineplot(x="datetime", y="min", data=df4, errorbar=None, lw=lw, label="Pullataujaq")
+sns.lineplot(x="datetime", y="min", data=df3, errorbar=None, lw=lw, hue="uid")
 ax.set(xlabel=None, ylabel="Voltage Min (V)")
 plt.xticks(rotation=45, horizontalalignment="center")
 ax.xaxis.set_major_formatter(mdates.DateFormatter(date_format))
 ax.xaxis.set_major_locator(mdates.MonthLocator(interval=1)) 
 ax.legend(loc="center left", bbox_to_anchor=(1.0, 0.5), title="Station")
 plt.savefig(path_figures + "voltage_min.png", dpi=dpi, transparent=False, bbox_inches="tight")
+
+
+
+# Add month column
+
+df["month"] = df["datetime"].dt.
+df["month"] = pd.to_datetime(df["month"], format='%m').dt.month_name().str.slice(stop=3)
+
+# relationship (°)
+fig, ax = plt.subplots(figsize=(10,5))
+ax.grid(ls="dotted")
+sns.scatterplot(x="wind_gust_speed", y="pitch", data=df, hue="month")
+ax.set(xlabel="Wind Gust Speed (km/h)", ylabel="Pitch (°)")
+plt.xticks(rotation=0, horizontalalignment="right")
+#ax.xaxis.set_major_formatter(mdates.DateFormatter(date_format))
+#ax.xaxis.set_major_locator(mdates.DayLocator(interval=7)) 
+ax.legend(loc="center left", bbox_to_anchor=(1.0, 0.5), title="Month")
+plt.savefig(path_figures + "pitch_gust2.png", dpi=dpi, transparent=False, bbox_inches="tight")
+
+
+
+
+fig, ax = plt.subplots(figsize=(10,5))
+ax.grid(ls="dotted")
+sns.scatterplot(x="wind_gust_speed", y="roll", data=df, label="Qakuqtaqtujut")
+ax.set(xlabel="Wind Gust Speed (km/h)", ylabel="Pitch (°)")
+plt.xticks(rotation=0, horizontalalignment="right")
+#ax.xaxis.set_major_formatter(mdates.DateFormatter(date_format))
+#ax.xaxis.set_major_locator(mdates.DayLocator(interval=7)) 
+ax.legend(loc="center left", bbox_to_anchor=(1.0, 0.5), title="Station")
+plt.savefig(path_figures + "roll_gust.png", dpi=dpi, transparent=False, bbox_inches="tight")
+
+
